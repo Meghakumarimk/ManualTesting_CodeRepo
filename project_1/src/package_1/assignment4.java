d878fdffe62a3a6afe1606c882ccf62916cdcd38
Megha Kumari <meghasah1642@gmail.com>
2024-08-10 00:43:45 +0530

    committed assignment4

---
+++ project_1/src/package_1/assignment4.java
@@ -0,0 +1,39 @@
+package package_1;
+
+public class assignment4 {
+	
+	public assignment4()
+	{
+		this(7,8,9);
+		System.out.println("Default constructor");
+	}
+	
+	public assignment4(int a)
+	
+	{
+		this(5,6);
+		System.out.println("One parameterised");
+	}
+	
+	public assignment4(int b,int c)
+	{
+		this();
+		System.out.println("Two parameterised");
+	}
+	public assignment4(int d,int f,int k) 
+	{
+	
+		System.out.println("Three parameterised");
+	}
+	
+	
+	
+	
+	public static void main(String[] args) 
+	{
+		assignment4 as4 = new assignment4(5);
+		
+	}
+	
+
+}
